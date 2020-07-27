91cb90f99a97d71da1cbdae97039ed87024afd3b
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2020-07-27 22:45:35 +0100

    updated 404

--- javascript/java404.js
+++ javascript/java404.js
@@ -1,9 +1,1 @@
-function animate() {
-    document.querySelector('blockquote').classList.toggle('animate');
-  }
-  
-  document.addEventListener('click', animate);
-  setTimeout(animate, 0);
-
-
 window.addEventListener('click', ()=> {window.location.href="/"})
