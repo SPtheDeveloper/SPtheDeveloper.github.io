4463f7175bdcbfcddf5c95ad86ee1236b97f629e
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2021-01-19 13:50:19 +0000

    added contact form

--- javascript/java.js
+++ javascript/java.js
@@ -19,3 +19,15 @@ const options = {
 };
 
 const typed = new Typed('#type', options);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  el.addEventListener('blur', e => {
+    if(e.target.value) {
+      e.target.classList.add('dirty');
+    } else {
+      e.target.classList.remove('dirty');
+    }
+  })
+})
