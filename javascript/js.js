24bb050474e717691ced8aff08d9202dff32e4b7
pwnker <github@pwnker.com>
2021-04-26 19:49:10 +0100

    added dropshadow

--- javascript/js.js
+++ javascript/js.js
@@ -38,6 +38,42 @@ inputs.forEach(el => {
 })
 
 
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame;
+
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+
+      // Call our function and pass any params we received
+      fn(...params);
+    });
+
+  }
+};
+
+
+// scroll show shadow
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+}
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+storeScroll();
+
+
+
+
+
  // pageclip
 var form = document.querySelector('.pageclip-form')
 Pageclip.form(form, {
