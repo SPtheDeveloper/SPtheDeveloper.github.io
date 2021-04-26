2d3c668bb94eb0ccf82d0d1cec45f6cfc9a666ca
pwnker <github@pwnker.com>
2021-04-26 21:54:33 +0100

    added background to headings

--- javascript/js.js
+++ javascript/js.js
@@ -38,32 +38,29 @@ inputs.forEach(el => {
 })
 
 
-// The debounce function receives our function as a parameter
+// scroll show shadow
 const debounce = (fn) => {
 
-  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+
   let frame;
 
-  // The debounce function returns a new function that can receive a variable number of arguments
   return (...params) => {
 
-    // If the frame variable has been defined, clear it now, and queue for next frame
+
     if (frame) {
       cancelAnimationFrame(frame);
     }
 
-    // Queue our function call for the next frame
+
     frame = requestAnimationFrame(() => {
 
-      // Call our function and pass any params we received
+
       fn(...params);
     });
 
   }
 };
 
-
-// scroll show shadow
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
 }
